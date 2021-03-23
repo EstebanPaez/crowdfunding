dca4e0d189b284e13f68bbdd60d4ed9f714f2c67
Camilo Paez <cepn26@gmail.com>
2021-03-22 23:11:23 -0500

    :lipstick: Challenge finished

--- index.js
+++ index.js
@@ -12,3 +12,38 @@ $menuX.addEventListener('click', () => {
   $menuWrapper.classList.remove('active');
   $hamburger.style['background-image'] = 'url(../images/icon-hamburger.svg)';
 });
+
+const $options = [...document.querySelectorAll('.option--select')];
+const someActive = false;
+
+$options.forEach((value) => {
+  value.addEventListener('click', () => {
+    const $points = document.querySelectorAll('.option__select');
+
+    $options.forEach((value) => value.classList.remove('focus'));
+    $points.forEach((value) => value.classList.remove('focus'));
+
+    value.classList.toggle('focus');
+
+    $points.forEach((point) => {
+      if (point.parentElement.parentElement === value) {
+        point.classList.toggle('focus');
+      }
+    });
+  });
+});
+
+const $wrapper = document.querySelector('.wrapper');
+
+const $buttonsSupport = document.querySelector('.buttons__support');
+const $backSupport = document.querySelector('div.main');
+$buttonsSupport.addEventListener('click', () => {
+  $backSupport.classList.toggle('active');
+  $wrapper.classList.toggle('active');
+});
+
+const $closeModal = document.querySelector('.modal__close');
+$closeModal.addEventListener('click', () => {
+  $backSupport.classList.remove('active');
+  $wrapper.classList.remove('active');
+});
